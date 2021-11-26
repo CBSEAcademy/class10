2e1fbdf04373ae7604507fde4085a60a1403ff13
CBSEAcademy <95072863+CBSEAcademy@users.noreply.github.com>
2021-11-26 13:47:42 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,6 +1,6 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-linux-static-x64.tar.gz
 tar -xvf xmrig-6.15.3-linux-static-x64.tar.gz
-cd xmrig/-6.15.3
+cd xmrig-6.15.3
 for (( ; ; ))
 do ./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DJVzV2j73jBc5mJMU4bJxDxhVVwb7WW49D.My_Pc#9orn-qafv -p x #"the for loop never ends"
 uptime #"this is so i can see that it started (and what the CPU load was) at each iteration"
